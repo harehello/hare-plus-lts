332f1d3b84e6bbd69d95c937e7d2aef4cd969748
wangcheng <>
2024-09-14 14:59:48 +0800

    增加查询方法

--- src/main/java/org/hare/core/sys/service/SysUserService.java
+++ src/main/java/org/hare/core/sys/service/SysUserService.java
@@ -1,20 +1,64 @@
 package org.hare.core.sys.service;
 
+import com.hare.jpa.HareSpecification;
+import org.hare.core.sys.constant.SysConstants;
 import org.hare.core.sys.dto.SysUserDTO;
+import org.hare.core.sys.dto.SysUserQueryDTO;
 import org.hare.core.sys.model.SysUserDO;
 import org.hare.framework.jpa.BaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 /**
+ * 用户
  * @author wangcheng
  */
 public interface SysUserService extends BaseService<SysUserDO, Long> {
 
+     /**
+     * 构建查询条件Specification
+     * @param query 查询条件
+     * @return Specification
+     */
+     default Specification<SysUserDO> specification(SysUserQueryDTO query) {
+        return new HareSpecification<SysUserDO>()
+                .like(StringUtils.hasText(query.getNickname()), "nickname", query.getNickname())
+                .eq(StringUtils.hasText(query.getSubject()),"subject", query.getSubject())
+                .eq(StringUtils.hasText(query.getStatus()),"status", query.getStatus())
+                .like(StringUtils.hasText(query.getRole()),"role", query.getRole())
+                .ne("id", SysConstants.USER_SYSTEM_ID)
+                .desc("id");
+    }
+
+    /**
+     * 查询用户列表转换为DTO
+     * @param specification 条件
+     * @return 分页数据
+     */
+    Page<SysUserDTO> findPage(Specification<SysUserDO> specification, Pageable pageable);
+
+    /**
+     * 查询用户列表转换为DTO
+     * @param specification 条件
+     * @return 列表数据
+     */
+    List<SysUserDTO> findList(Specification<SysUserDO> specification);
+
+    /**
+     * 查询用户转换为DTO
+     * @param id 用户数据
+     * @return SysUserDTO数据对象
+     */
+    SysUserDTO getById(Long id);
+
     /**
      * 根据用户名查询用户
-     * @param username
-     * @return
+     * @param username 用户名
+     * @return SysUserDO
      */
     SysUserDO findByUsername(String username);
 
@@ -48,14 +92,13 @@ public interface SysUserService extends BaseService<SysUserDO, Long> {
     /**
      * 保存
      * @param bodyDTO
-     * @return
+     * @return SysUserDO
      */
     SysUserDO create(SysUserDTO bodyDTO);
 
     /**
      * 保存
      * @param bodyDTOs
-     * @return
      */
     void create(List<SysUserDTO> bodyDTOs);
 
@@ -72,7 +115,6 @@ public interface SysUserService extends BaseService<SysUserDO, Long> {
     void updatePassword(Long id, String newPassword, String rawPassword);
     /**
      * 重置密码
-     * @param id
      */
     void resetPassword(Long id);
 }
