f49d1b946204e0577edb0f68e356bae5a376e2a3
wangcheng <>
2025-07-04 09:36:49 +0800

    去掉注解

--- src/main/java/org/hare/core/sys/repository/SysJobRepository.java
+++ src/main/java/org/hare/core/sys/repository/SysJobRepository.java
@@ -4,7 +4,6 @@ import org.hare.core.sys.model.SysJobDO;
 import org.hare.framework.jpa.BaseRepository;
 
 /**
- * sys Repository
  * @author wangcheng
  */
 public interface SysJobRepository extends BaseRepository<SysJobDO, Long> {
